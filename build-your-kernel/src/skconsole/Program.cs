56238174316963018a242fe7a676feff9de2c7e4
tiagobaranzelli <tiago.788@gmail.com>
2024-09-21 11:45:53 -0300

    feat: build-your-kernel

---
+++ build-your-kernel/src/skconsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.SemanticKernel;
+using DotNetEnv;
+
+Env.Load();
+
+string AZURE_OPENAI_ENDPOINT = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new ArgumentNullException("AZURE_OPENAI_ENDPOINT");
+string AZURE_OPENAI_API_KEY = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? throw new ArgumentNullException("AZURE_OPENAI_API_KEY");
+
+// Create kernel
+var builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(
+    deploymentName: "gpt-35-turbo-16k",
+    endpoint: AZURE_OPENAI_ENDPOINT,
+    apiKey: AZURE_OPENAI_API_KEY,
+    modelId: "gpt-35-turbo-16k" // optional
+);
+var kernel = builder.Build();
+
+
+var result = await kernel.InvokePromptAsync(
+        "Give me a list of breakfast foods with eggs and cheese and give me answer in portuguese");
+Console.WriteLine(result);
+
+Console.ReadLine();
